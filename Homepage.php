d7441d4c9bfabea02482029078496baffaa8ad04
r20181114 <20181114@novaims.unl.pt>
2020-11-19 17:03:15 +0000

    Primeiro Update

---
+++ Homepage.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Projeto Amik@</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+
+        <link rel="stylesheet" type="text/css" href="CSS/Amik@.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+        <header>
+
+            <table class="tabelaHeader" >
+
+                <tr>
+                    <th >
+                        
+                        <a  href="Profile.php" style="text-decoration:none;"> 
+                            <img src="imagens\profile.png" alt="Profile Icon" class="IconHeader"></img>
+                            <p class="hyperlink">PERFIL</p> 
+                        </a>
+                        
+    
+                    </th>
+
+                    <th>
+                        <img src="Imagens\LogoAmika.png" alt="Logo Amik@" class="logoAmika"></img>
+                    </th>
+
+                    <th>
+                        <a  href="" style="text-decoration:none;"> 
+                            <img src="imagens\home.png" alt="Home Icon" class="IconHeader"></img>
+                            <p class="hyperlink">MENU</p> 
+                        </a>
+                    </th>
+
+                    <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\Logout.png" alt="Logout Icon" class="IconHeader"></img>
+                            <p class="hyperlink">SAIR</p> 
+                        </a>
+                    </th>
+
+                </tr>
+
+            </table>
+
+        </header>
+
+        <table class="tabelaBody">
+            <tr>
+                
+
+                <th>
+                    <a  href="" style="text-decoration:none;">
+                        <img src="imagens\Agenda.png" alt="Agenda Icon" class="Icon"></img>
+                        <p class="hyperlink">AGENDA</p> 
+                    </a>
+                </th>
+
+                <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\Forum.png" alt="Forum Icon" class="Icon"></img>
+                            <p class="hyperlink">FORUM</p> 
+                        </a>
+                </th>
+
+                <th>
+                    <a  href="" style="text-decoration:none;">
+                            <img src="imagens\Chat.png" alt="Chat Icon" class="Icon"></img>
+                            <p class="hyperlink">CHAT</p> 
+                    </a>
+                </th>
+                    
+                <th>
+                    <a  href="" style="text-decoration:none;">
+                        <img src="imagens\Pesquisar.png" alt="Pesquisar Icon" class="Icon"></img>
+                        <p class="hyperlink">PESQUISAR</p> 
+                    </a>
+                </th>
+                    
+                    <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\Exercicios.png" alt="Exercicios Icon" class="Icon"></img>
+                            <p class="hyperlink">EXERCICIOS</p> 
+                        </a>
+                    </th>
+                
+            </tr>
+
+            <tr>
+                    <th>
+                    </th>
+
+                    <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\Emergencia.png" alt="Emergencia Icon" class="Icon"></img>
+                            <p class="hyperlink">DICAS EMERGÊNCIA</p> 
+                        </a>
+                    </th>
+
+                    <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\informacoes.png" alt="Informações Icon" class="Icon"></img>
+                            <p class="hyperlink">INFORMAÇÕES</p> 
+                        </a>
+                    </th>
+
+                    <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\pontos_interesse.png" alt="Pontos de Interesse Icon" class="Icon"></img>
+                            <p class="hyperlink">PONTOS DE INTERESSE</p> 
+                        </a>
+                    </th>
+
+                    <th>
+                        <a  href="" style="text-decoration:none;">
+                            <img src="imagens\apoio_voluntarios.png" alt="Apoio a VOluntários Icon" class="Icon"></img>
+                            <p class="hyperlink">APOIO A VOLUNTÁRIOS</p> 
+                        </a>
+                    </th>
+            </tr>
+        </table>
+       
+    </body>
+</html>
---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'Amik@';
+
+$mysqli = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+
