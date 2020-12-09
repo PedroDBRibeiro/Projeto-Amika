df198a173b23b98690221657a7f9186d42423bca
goncaloperdigon <perdigao10silva@gmail.com>
2020-12-09 13:22:27 +0000

    search2

--- location&hobbies.php
+++ location&hobbies.php
@@ -22,8 +22,6 @@ if (isset($_POST["hobbies"]))
  
   //$rows = mysqli_num_rows($result);
 
-//2PuzzlesCozinharComprasPuzzlesCozinharCompras3LerPuzzlesLerPuzzles
-
   while($row = mysqli_fetch_array($result)){
     echo $row['id_utilizador']; echo " " ; 
 
@@ -32,12 +30,35 @@ if (isset($_POST["hobbies"]))
       if($hobbie == $row['hobbie']){
       echo $row['hobbie']; echo " ";  
     }
-
       if($hobbie == $row['hobbie2']){
       echo $row['hobbie2']; echo " ";
     }
       if($hobbie == $row['hobbie3']){
       echo $row['hobbie3']; echo " ";
+    }
+    if($hobbie == $row['hobbie4']){
+      echo $row['hobbie4']; echo " ";
+    }
+    if($hobbie == $row['hobbie5']){
+      echo $row['hobbie5']; echo " ";
+    }
+    if($hobbie == $row['hobbie6']){
+      echo $row['hobbie6']; echo " ";
+    }
+    if($hobbie == $row['hobbie7']){
+      echo $row['hobbie7']; echo " ";
+    }
+    if($hobbie == $row['hobbie8']){
+      echo $row['hobbie8']; echo " ";
+    }
+    if($hobbie == $row['hobbie9']){
+      echo $row['hobbie9']; echo " ";
+    }
+    if($hobbie == $row['hobbie10']){
+      echo $row['hobbie10']; echo " ";  
+    }
+    if($hobbie == $row['hobbie11']){
+      echo $row['hobbie11']; echo " ";  
     }
 
     }
