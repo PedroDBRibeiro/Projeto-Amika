ee38e92fe5ca3bd77383fcdb747a51aea8fe083b
inesmargarido <inesom1999@gmail.com>
2020-12-23 00:27:23 +0000

    search working

--- delete_activities.php
+++ delete_activities.php
@@ -1,12 +1,13 @@
 <?php
 
-include ("config.php");
 
 if(isset($_POST["id"])) 
 {
-/*$query = 'DELETE from atividades WHERE ID_ATIVIDADE=:id'; */
-
- $statement = $mysqli->prepare('DELETE from atividades WHERE id_atividade=:id');
+ $connect = new PDO('mysql:host=localhost;dbname=amik@', 'root', '');
+ $query = "
+ DELETE from atividades WHERE ID_ATIVIDADE=:id
+ ";
+ $statement = $connect->prepare($query);
  $statement->execute(
   array(
    ':id' => $_POST['id'] 
--- load_activities.php
+++ load_activities.php
@@ -1,13 +1,16 @@
 <?php
 
 //load_activities.php
-include ("config.php");
+
+$connect = new PDO('mysql:host=localhost;dbname=amik@', 'root', '');
 
 $data = array();
 
-/*$query = "SELECT * FROM atividades ORDER BY id_atividade";*/
+$query = "SELECT a.*, c.CATEGORIA FROM atividades as a, categoria as c
+        WHERE a.ID_CATEGORIA = c.ID_CATEGORIA
+        ORDER BY ID_ATIVIDADE";
 
-$statement = $mysqli->prepare("SELECT * FROM atividades ORDER BY id_atividade");
+$statement = $connect->prepare($query);
 
 $statement->execute();
 
@@ -16,12 +19,13 @@ $result = $statement->fetchAll();
 foreach($result as $row) 
 {
  $data[] = array(
-  'id'   => $row["id_atividade"],
-  'title'   => $row["categoria"],
-  'start'   => $row["data_inicio"],
-  'end'   => $row["data_fim"]
+  'id'   => $row["ID_ATIVIDADE"],
+  'title'   => $row["CATEGORIA"],
+  'start'   => $row["DATA_INICIO"],
+  'end'   => $row["DATA_FIM"]
  );
 }
+
 
 echo json_encode($data);
 
